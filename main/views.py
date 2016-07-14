a1678fe9dede42f257d85e9357c1aa0e45d51aa7
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-14 19:19:45 +0900

    post_list view 구현

--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def main(request):
-    context = {
 
+    post_list = Post.objects.all()
+    context = {
+        'post_list': post_list
     }
     return render(request, "main/home.html", context)
