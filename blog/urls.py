c7bdc78de2e31ec6dd89a8d789069778e7bfd2da
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-15 15:13:13 +0900

    urls.py 오류 수정

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,10 @@ from blog import views
 
 urlpatterns = [
 
-    url(r'^blog/$', views.post_list, name='post_list'),
-    url(r'^blog/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^blog/new/$', views.post_new, name='post_new'),
-    url(r'^blog/(?P<pk>\d+)/$', views.post_edit, name='post_edit'),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^new/$', views.post_new, name='post_new'),
+    url(r'^(?P<pk>\d+)/$', views.post_edit, name='post_edit'),
 
 
 	]
