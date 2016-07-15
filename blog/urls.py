4bf1a9ab7bcdbcf5a4edf5d0008b3b8f1ef5360c
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-15 14:27:01 +0900

    blog app views 함수 생성 및 url 연결

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
+from blog import views
+
 
 urlpatterns = [
 
+    url(r'^blog/$', views.post_list, name='post_list'),
+    url(r'^blog/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^blog/new/$', views.post_new, name='post_new'),
+    url(r'^blog/(?P<pk>\d+)/$', views.post_edit, name='post_edit'),
 
 
-]
+	]
--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
+
+def post_new(request):
+	context = {}
+	return render(request, 'blog/post_new.html', context)
+
+
+def post_edit(request):
+	context = {}
+	return render(request, 'blog/post_edit.html', context)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/post_detail.html', context)
+
+
+def post_list(request):
+    post_list = Post.objects.all()
+    context = {
+        'post_list': post_list,
+    }
+    return render(request, 'blog/post_list.html', context)
